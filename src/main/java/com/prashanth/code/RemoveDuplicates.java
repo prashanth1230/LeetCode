c73202acef447fe2c77eb98db3e97e5b8c6f9faa
Udupa, Prashanth <prashanth1230@gmail.com>
2021-08-04 19:36:54 +0530

    Trying out a remove duplicate example.

---
+++ src/main/java/com/prashanth/code/RemoveDuplicates.java
@@ -0,0 +1,9 @@
+package com.prashanth.code;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+
+        return nums.length;
+    }
+}
---
+++ src/test/java/com/prashanth/code/RemoveDuplicatesTest.java
@@ -0,0 +1,22 @@
+package com.prashanth.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesTest {
+
+    @Test
+    void removeDuplicates() {
+        RemoveDuplicates obj = new RemoveDuplicates();
+        int[] firstInput = new int[]{1,1,2};
+        assertEquals(2, obj.removeDuplicates(firstInput));
+        assertEquals(firstInput[0], 1);
+        assertEquals(firstInput[1], 2);
+        int[] secondInput = new int[]{0,0,1,1,1,2,2,3,3,4};
+        //assertEquals(5, obj.removeDuplicates(secondInput));
+        assertEquals(0, secondInput[0]);
+        assertEquals(1, secondInput[1]);
+        assertEquals(2, secondInput[2]);
+    }
+}
