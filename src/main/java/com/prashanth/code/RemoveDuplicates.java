668f24b74e5efc75fdb56db3a1698547f0d60d2e
Udupa, Prashanth <prashanth1230@gmail.com>
2021-09-02 16:46:18 +0530

    Completed remove dulicates example. Also downgraded to use jdk 11.

--- src/main/java/com/prashanth/code/RemoveDuplicates.java
+++ src/main/java/com/prashanth/code/RemoveDuplicates.java
@@ -2,8 +2,14 @@ package com.prashanth.code;
 
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
-        int count = 0;
-
-        return nums.length;
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 }
--- src/test/java/com/prashanth/code/RemoveDuplicatesTest.java
+++ src/test/java/com/prashanth/code/RemoveDuplicatesTest.java
@@ -14,7 +14,7 @@ class RemoveDuplicatesTest {
         assertEquals(firstInput[0], 1);
         assertEquals(firstInput[1], 2);
         int[] secondInput = new int[]{0,0,1,1,1,2,2,3,3,4};
-        //assertEquals(5, obj.removeDuplicates(secondInput));
+        assertEquals(5, obj.removeDuplicates(secondInput));
         assertEquals(0, secondInput[0]);
         assertEquals(1, secondInput[1]);
         assertEquals(2, secondInput[2]);
